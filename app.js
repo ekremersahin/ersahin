f4a0ba2a1d8a26270d54791904868411b98edf11
ekremersahin <ekremersahin@outlook.com>
2022-02-07 14:53:46 +0300

    ss

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ var main = new movico.Component('div');
 main.element.innerHTML = `<div>
                             <h1>Ekrem Erşahin</h1>
                             <p>Hi everybody. <br/> I currently don't have any time for my website development. I hope we'll see you soon.</p>
+                            <p>e-mail: ekrem@ersahin.net</p>
+                            <p>whatsapp: +90(530) 963 49 26</p>
                         </div>` ;
 main.renderTo(document.body);
