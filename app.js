527ab47a82eb60e1984a0db7e0a5a7d943b85620
ekremersahin <ekremersahin@outlook.com>
2022-02-07 14:47:09 +0300

    latest

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-var express = require('express');
-var app = express();
-
-// on the request to root (localhost:3000/)
-app.get('/', function (req, res) {
-    res.send('<b>My</b> first express http server');
-});
-
-// On localhost:3000/welcome
-app.get('/welcome', function (req, res) {
-    res.send('<b>Hello</b> welcome to my http server made with express');
-});
-
-// Change the 404 message modifing the middleware
-app.use(function(req, res, next) {
-    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-});
-
-// start the server in the port 3000 !
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000.');
-});
+ 
+ var data = {
+    title:'Ekrem Erşahin'
+ } 
+ var Db = new movico.Observer(data); 
+var main = new movico.Component('div');
+main.element.innerHTML = `<div>
+                            <h1>Ekrem Erşahin</h1>
+                            <p>Hi everybody. <br/> I currently have't time and not working to my website development. I hope we'll see you soon.</p>
+                        </div>` ;
+main.renderTo(document.body);
